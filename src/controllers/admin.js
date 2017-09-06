1ca58c24c078918419b0c7e1aef82a488e5c748e
baotang <tlyspa@gmail.com>
2017-09-07 00:41:14 +0800

    create first user

--- src/controllers/admin.js
+++ src/controllers/admin.js
@@ -2,6 +2,7 @@ import Admin from '../models/admin'
 // import {print} from '../utils'
 export let login = async (ctx)=>{
     let { username, password } = ctx.request.body;
+    console.log({ username, password })
     let user = await Admin.findOne({username});
     if(user && user.authenticate(password)){
         ctx.session.user = user
@@ -13,10 +14,30 @@ export let login = async (ctx)=>{
             }
         }
     }else{
-        ctx.status = 401;
-        ctx.body = {
-            msg: 'username or password is not right'
+        let number = await Admin.count()
+        if(!number){
+            let { username, password } = ctx.request.body;
+            let user = await new Admin({username,password}).save()
+            ctx.session.user = user
+            console.log(user)
+            ctx.status = 200;
+            ctx.body = {
+                msg: 'login success',
+                data:{
+                    user:{
+                        username: user.username,
+                        _id: user._id
+                    }
+                    
+                }
+            }
+        }else{
+            ctx.status = 401;
+            ctx.body = {
+                msg: 'username or password is not right'
+            }
         }
+        
     }
 }
 export let loginout = async (ctx)=>{

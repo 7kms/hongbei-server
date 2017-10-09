cd32bc20b10b0d52aa0fc1d247c2256e030d0cbc
baotang <tlyspa@gmail.com>
2017-10-10 01:07:16 +0800

    fix

--- src/controllers/upload.js
+++ src/controllers/upload.js
@@ -1,13 +1,11 @@
 import fs from 'fs';
 import path from 'path';
-import os from 'os';
-import {print} from '../utils';
+// import os from 'os';
+// import {print} from '../utils';
 import uuidv1 from 'uuid/v1';
 let storePath = path.resolve(__dirname,'../../uploads/images')
 export let upload = async (ctx, next)=>{
-  console.log(ctx.request)
   const file = ctx.request.body.files.file;
-  console.log(ctx.request.body.files)
   let ext = file.type.split('/')[1] || file.name.replace(/^.*\./g,'') || '';
   let pathuuid = uuidv1();
   if(!fs.existsSync(storePath)){
@@ -16,6 +14,7 @@ export let upload = async (ctx, next)=>{
   const str = `${storePath}/${pathuuid}.${ext}`
   const reader = fs.createReadStream(file.path);
   const stream = fs.createWriteStream(str);
+  console.log(str)
   reader.pipe(stream);
   ctx.status = 200;
   ctx.body = {

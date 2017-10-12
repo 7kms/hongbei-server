d74a2ecb1c2f0e0b4ef359050c4eb4027d5e2ae2
baotang <tlyspa@gmail.com>
2017-10-12 11:00:05 +0800

    course crud

--- src/routes/course.js
+++ src/routes/course.js
@@ -1,8 +1,9 @@
 import Router from 'koa-router';
-import { itemInsert,itemUpdate,itemDelete,itemList } from '../controllers/course';
+import { itemInsert,itemUpdate,itemDelete,itemList,itemDetail } from '../controllers/course';
 import { verify } from '../controllers/token';
 let router = new Router();
 router.get('/', itemList)
+router.get('/:id',itemDetail)
 router.post('/',verify, itemInsert)
 router.put('/:id',verify, itemUpdate)
 router.delete('/:id',verify, itemDelete)

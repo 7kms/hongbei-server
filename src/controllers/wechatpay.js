c1f809da7e32bc066e1d3cccb17d8d66aaaff14b
baotang <tlyspa@gmail.com>
2017-11-04 17:02:29 +0800

    notify

--- src/controllers/wechatpay.js
+++ src/controllers/wechatpay.js
@@ -3,9 +3,11 @@ import {parseXml} from '../utils';
 // import Order from '../models/client/order';
 
 export const notify = async (ctx)=>{
+    console.log('============notify===============')
     console.log(ctx.request.body)
     let obj = parseXml(ctx.request.body)
     console.log(obj);
+    console.log('============notify end===============')
     // let {appid,mch_id,sign} = obj;
     ctx.body={
         data:obj
